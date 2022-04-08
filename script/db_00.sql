a8c2b75fb0885545a019e09e771e50486c532fdb
aoheng <kemingheng@angogo.cn>
2022-04-08 15:43:52 +0800

    feat: id类型由int调整为BigInt

--- script/db_00.sql
+++ script/db_00.sql
@@ -1,112 +1,116 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : localhost-3306
-Source Server Version : 50621
-Source Host           : 127.0.0.1:3306
-Source Database       : marketingdb_00
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80026
+ Source Host           : localhost:3306
+ Source Schema         : db_00
 
-Target Server Type    : MYSQL
-Target Server Version : 50621
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 80026
+ File Encoding         : 65001
 
-Date: 2019-03-26 22:52:10
+ Date: 08/04/2022 15:31:32
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for t_new_order_0000
 -- ----------------------------
 DROP TABLE IF EXISTS `t_new_order_0000`;
-CREATE TABLE `t_new_order_0000` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(128) DEFAULT NULL,
-  `order_id` varchar(128) DEFAULT NULL,
-  `user_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1360 DEFAULT CHARSET=utf8;
+CREATE TABLE `t_new_order_0000`  (
+                                     `id` bigint NOT NULL AUTO_INCREMENT,
+                                     `user_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                                     `order_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                                     `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                                     PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for t_new_order_0001
 -- ----------------------------
 DROP TABLE IF EXISTS `t_new_order_0001`;
-CREATE TABLE `t_new_order_0001` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(128) DEFAULT NULL,
-  `order_id` varchar(128) DEFAULT NULL,
-  `user_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1370 DEFAULT CHARSET=utf8;
+CREATE TABLE `t_new_order_0001`  (
+                                     `id` bigint NOT NULL AUTO_INCREMENT,
+                                     `user_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                                     `order_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                                     `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                                     PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for t_order_0000
 -- ----------------------------
 DROP TABLE IF EXISTS `t_order_0000`;
-CREATE TABLE `t_order_0000` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(20) DEFAULT NULL,
-  `order_id` bigint(20) DEFAULT NULL,
-  `user_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1239 DEFAULT CHARSET=utf8;
+CREATE TABLE `t_order_0000`  (
+                                 `id` bigint NOT NULL AUTO_INCREMENT,
+                                 `user_id` bigint NULL DEFAULT NULL,
+                                 `order_id` bigint NULL DEFAULT NULL,
+                                 `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                                 PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1255 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for t_order_0001
 -- ----------------------------
 DROP TABLE IF EXISTS `t_order_0001`;
-CREATE TABLE `t_order_0001` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(32) DEFAULT NULL,
-  `order_id` bigint(32) DEFAULT NULL,
-  `user_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=330 DEFAULT CHARSET=utf8;
+CREATE TABLE `t_order_0001`  (
+                                 `id` bigint NOT NULL AUTO_INCREMENT,
+                                 `user_id` bigint NULL DEFAULT NULL,
+                                 `order_id` bigint NULL DEFAULT NULL,
+                                 `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                                 PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for t_user_0000
 -- ----------------------------
 DROP TABLE IF EXISTS `t_user_0000`;
-CREATE TABLE `t_user_0000` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(128) NOT NULL,
-  `user_name` varchar(255) DEFAULT NULL,
-  `user_out_id` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `t_user_0000`  (
+                                `id` bigint NOT NULL AUTO_INCREMENT,
+                                `user_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+                                `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                                `user_out_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                                PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for t_user_0001
 -- ----------------------------
 DROP TABLE IF EXISTS `t_user_0001`;
-CREATE TABLE `t_user_0001` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(128) NOT NULL,
-  `user_name` varchar(255) DEFAULT NULL,
-  `user_out_id` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `t_user_0001`  (
+                                `id` bigint NOT NULL AUTO_INCREMENT,
+                                `user_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+                                `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                                `user_out_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                                PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for t_user_0002
 -- ----------------------------
 DROP TABLE IF EXISTS `t_user_0002`;
-CREATE TABLE `t_user_0002` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(128) NOT NULL,
-  `user_name` varchar(255) DEFAULT NULL,
-  `user_out_id` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `t_user_0002`  (
+                                `id` bigint NOT NULL AUTO_INCREMENT,
+                                `user_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+                                `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                                `user_out_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                                PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for t_user_0003
 -- ----------------------------
 DROP TABLE IF EXISTS `t_user_0003`;
-CREATE TABLE `t_user_0003` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(128) NOT NULL,
-  `user_name` varchar(255) DEFAULT NULL,
-  `user_out_id` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `t_user_0003`  (
+                                `id` bigint NOT NULL AUTO_INCREMENT,
+                                `user_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+                                `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                                `user_out_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+                                PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
--- src/main/java/com/snowalker/shardingjdbc/snowalker/demo/entity/OrderInfo.java
+++ src/main/java/com/snowalker/shardingjdbc/snowalker/demo/entity/OrderInfo.java
@@ -1,62 +1,20 @@
 package com.snowalker.shardingjdbc.snowalker.demo.entity;
 
-/**
- * @author snowalker
- * @version 1.0
- * @date 2019/3/3 22:42
- * @className OrderInfo
- * @desc
- */
-public class OrderInfo {
+import lombok.Data;
 
-    private String id;
+import java.io.Serializable;
+
+/**
+ * @description: 订单实体
+ * @author: Aoheng
+ * @date: 2022/4/2 15:15
+ */
+@Data
+public class OrderInfo implements Serializable {
+
+    private Long id;
     private Long userId;
     private Long orderId;
     private String userName;
 
-    public String getId() {
-        return id;
-    }
-
-    public OrderInfo setId(String id) {
-        this.id = id;
-        return this;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public OrderInfo setUserId(Long userId) {
-        this.userId = userId;
-        return this;
-    }
-
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public OrderInfo setOrderId(Long orderId) {
-        this.orderId = orderId;
-        return this;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public OrderInfo setUserName(String userName) {
-        this.userName = userName;
-        return this;
-    }
-
-    @Override
-    public String toString() {
-        return "OrderInfo{" +
-                "id='" + id + '\'' +
-                ", userId=" + userId +
-                ", orderId=" + orderId +
-                ", userName='" + userName + '\'' +
-                '}';
-    }
 }
